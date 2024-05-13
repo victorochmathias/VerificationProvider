390de3a3f32eee84a8b01ec7f23082eed3dcc73b
Mathias Rönnmark <mathiasronnmark@hotmail.com>
2024-05-13 15:46:54 +0200

    GenerateVerificationCode completed

---
+++ VerificationProvider/Services/VerificationService.cs
@@ -0,0 +1,131 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using VerificationProvider.Data.Contexts;
+using VerificationProvider.Models;
+
+namespace VerificationProvider.Services;
+
+public class VerificationService(ILogger<VerificationService> logger, IServiceProvider serviceProvider) : IVerificationService
+{
+    private readonly ILogger<VerificationService> _logger = logger;
+    private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+    public VerificationRequest UnpackVerificationRequest(ServiceBusReceivedMessage message)
+    {
+        try
+        {
+            var verificationRequest = JsonConvert.DeserializeObject<VerificationRequest>(message.Body.ToString());
+            if (verificationRequest != null && !string.IsNullOrEmpty(verificationRequest.Email))
+                return verificationRequest;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.UnpackVerificationRequest() :: {ex.Message}");
+        }
+
+        return null!;
+    }
+
+    public string GenerateCode()
+    {
+        try
+        {
+            var rnd = new Random();
+            var code = rnd.Next(100000, 999999);
+
+            return code.ToString();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.GenerateCode() :: {ex.Message}");
+        }
+
+        return null!;
+    }
+
+    public async Task<bool> SaveVerificationRequest(VerificationRequest verificationRequest, string code)
+    {
+        try
+        {
+            using var context = _serviceProvider.GetRequiredService<DataContext>();
+
+            var existingRequest = await context.VerificationRequests.FirstOrDefaultAsync(x => x.Email == verificationRequest.Email);
+            if (existingRequest != null)
+            {
+                existingRequest.Code = code;
+                existingRequest.ExpiryDate = DateTime.Now.AddMinutes(5);
+                context.Entry(existingRequest).State = EntityState.Modified;
+            }
+            else
+            {
+                context.VerificationRequests.Add(new Data.Entities.VerificationRequestEntity() { Email = verificationRequest.Email, Code = code });
+            }
+
+            await context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.SaveVerificationRequest() :: {ex.Message}");
+        }
+
+        return false;
+    }
+
+    public EmailRequest GenerateEmailRequest(VerificationRequest verificationRequest, string code)
+    {
+        try
+        {
+            if (!string.IsNullOrEmpty(verificationRequest.Email) && !string.IsNullOrEmpty(code))
+            {
+                var emailRequest = new EmailRequest()
+                {
+                    To = verificationRequest.Email,
+                    Subject = $"Verification Code {code}",
+                    HtmlBody = $@"
+                    <html lang='en'>
+                        <head>
+                            <meta charset='UTF-8'>
+                            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                            <title>Verification code</title>
+                        </head>
+                        <body>
+                            <h2>Din verifieringskod</h2>
+                            <p>Din verifieringskod är: <strong>{code}</strong></p>
+                            <p>Använd denna kod för att slutföra din verifiering.</p>
+                        </body>
+                    </html>
+                    ",
+                    PlainText = $"Din verifieringskod är : {code}"
+                };
+
+                return emailRequest;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.GenerateEmailRequest :: {ex.Message}");
+        }
+
+        return null!;
+    }
+
+    public string GenerateServiceBusEmailRequest(EmailRequest emailRequest)
+    {
+        try
+        {
+            var payload = JsonConvert.SerializeObject(emailRequest);
+            if (!string.IsNullOrEmpty(payload))
+                return payload;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.GenerateServiceBusEmailRequest() :: {ex.Message}");
+        }
+
+        return null!;
+    }
+}
