da20925f42ab9362fc296523a7f8f95540c9186d
Mathias Rönnmark <mathiasronnmark@hotmail.com>
2024-05-14 10:58:42 +0200

    ValidateVerificationCode completed

--- VerificationProvider/Program.cs
+++ VerificationProvider/Program.cs
@@ -16,6 +16,7 @@ var host = new HostBuilder()
         services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("VerificationRequestDataBase")));
         services.AddScoped<IVerificationService, VerificationService>();
         services.AddScoped<IVerificationCleanerService, VerificationCleanerService>();
+        services.AddScoped<IValidateVerificationCodeService, ValidateVerificationCodeService>();
     })
     .Build();
 
---
+++ VerificationProvider/Services/ValidateVerificationCodeService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using VerificationProvider.Data.Contexts;
+using VerificationProvider.Models;
+
+namespace VerificationProvider.Services;
+
+public class ValidateVerificationCodeService(ILogger<ValidateVerificationCodeService> logger, DataContext context) : IValidateVerificationCodeService
+{
+    private readonly ILogger<ValidateVerificationCodeService> _logger = logger;
+    private readonly DataContext _context = context;
+
+    public async Task<bool> ValidateCodeAsync(ValidateRequest validateRequest)
+    {
+        try
+        {
+            var entity = await _context.VerificationRequests.FirstOrDefaultAsync(x => x.Email == validateRequest.Email && x.Code == validateRequest.Code);
+            if (entity != null)
+            {
+                _context.VerificationRequests.Remove(entity);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : ValidateVerificationCode.ValidateCodeAsync() :: {ex.Message}");
+        }
+
+        return false;
+    }
+
+    public async Task<ValidateRequest> UnpackValidateRequestAsync(HttpRequest req)
+    {
+        try
+        {
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            if (!string.IsNullOrEmpty(body))
+            {
+                var validateRequest = JsonConvert.DeserializeObject<ValidateRequest>(body);
+                if (validateRequest != null)
+                    return validateRequest;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : ValidateVerificationCode.UnpackValidateRequestAsync() :: {ex.Message}");
+        }
+
+        return null!;
+    }
+
+
+}
